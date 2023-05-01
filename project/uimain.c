b980a883df626f616a6be49d260af4d7ae6a103b
Jacob <jagonzalez63@miners.utep.edu>
2023-05-01 11:51:54 -0600

    shapes added

--- project/uimain.c
+++ project/uimain.c
@@ -12,6 +12,52 @@
 
 #define SWITCHES 15
 
+
+short redrawScreen = 1;
+
+void wdt_c_handler()
+{
+  
+}
+
+void drawSquare(int x, int y, int size) {
+  for (int i = 0; i<size; i++) {
+    drawPixel(x-(size/2),y-(size/2)+i,COLOR_RED);
+    drawPixel(x+(size/2),y-(size/2)+i,COLOR_RED);
+    drawPixel(i+x-(size/2),y-(size/2),COLOR_RED);
+    drawPixel(i+x-(size/2),y+(size/2),COLOR_RED);
+  }
+}
+
+void drawTriangle(int x, int y, int size) {
+  //drawPixel(screenWidth/2, screenHeight/2-10, COLOR_RED);
+  for (int i = 0; i<size; i++) {
+    drawPixel(x-i,y-(size/2)+i, COLOR_RED);
+    drawPixel(x+i, y-(size/2)+i, COLOR_RED);
+    drawPixel(x-i, y+(size/2), COLOR_RED);
+    drawPixel(x+size-i, y+(size/2), COLOR_RED);
+  }
+}
+
+void drawX(int x, int y, int size) {
+  for (int i = 0; i<size; i++) {
+    drawPixel(x-i+(size/2), y+i, COLOR_RED);
+    drawPixel(x+i-(size/2), y+i, COLOR_RED);
+  }
+}
+
+void drawRectangle(int x, int y, int size) {
+  for (int i = 0; i<size; i++) {
+    drawPixel(x-(size/2),y-(size/2)+i,COLOR_RED);
+    drawPixel(x+(size/2),y-(size/2)+i,COLOR_RED);
+    drawPixel(x-(size/2),y+i,COLOR_RED);
+    drawPixel(x+(size/2),y+i,COLOR_RED);
+    drawPixel(i+x-(size/2),y-(size/2),COLOR_RED);
+    drawPixel(i+x-(size/2),y+size,COLOR_RED);
+  }
+}
+
+void 
 main()
 {
   configureClocks();
@@ -20,7 +66,8 @@ main()
 
   clearScreen(COLOR_BLUE);
 
-  for (int i=0; i< 20; i++) {
-    drawPixel(i,i,COLOR_RED);
-  }
+  drawSquare(15, screenHeight-15, 14);
+  drawTriangle(45,screenHeight-15, 12);
+  drawX(75, screenHeight-22, 14);
+  drawRectangle(105, screenHeight-22, 14);
 }
